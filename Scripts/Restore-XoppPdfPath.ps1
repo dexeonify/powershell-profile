10530ff6037f28ba68655deec42ab2706edac570
dexeonify <72267349+dexeonify@users.noreply.github.com>
2026-01-02 21:04:56 +0800

    Add description, credits and cleanup

--- Scripts/Restore-XoppPdfPath.ps1
+++ Scripts/Restore-XoppPdfPath.ps1
@@ -1,15 +1,23 @@
+<#
+    .SYNOPSIS
+    A script to convert the absolute PDF path in old Xournal++ documents to
+    relative path, so that they can be opened directly without going through a
+    dialog. It can even prevent crashes in later versions of Xournal++!
+#>
+
 param ($Path)
 
 $File = Get-Item $Path
 $Dir = $File.DirectoryName
-$Backup = $File.Name + ".bak"
-$Xml = Join-Path $Dir "$($File.BaseName).xml"
-$Xopp = Join-Path $Dir "$($File.BaseName).xopp"
 
-Write-Host "Extract xopp (gzipped XML file)" -ForegroundColor Green
+Write-Host "Extract xopp file (gzipped XML)" -ForegroundColor Green
+$Xml = Join-Path $Dir "$($File.BaseName).xml"
+# Specifiy filename when extracting using 7zip
+# https://stackoverflow.com/a/62881726/16689935
 7z.exe e $File -so > $Xml
 
 Write-Host "Backup original xopp file" -ForegroundColor Green
+$Backup = $File.Name + ".bak"
 Rename-Item $File $Backup
 
 Write-Host "Parse XML and correct absolute paths" -ForegroundColor Green
@@ -19,4 +27,5 @@ $Node.filename = $File.BaseName + ".pdf"
 $XmlDoc.Save($Xml)
 
 Write-Host "Repack XML to xopp file" -ForegroundColor Green
+$Xopp = Join-Path $Dir "$($File.BaseName).xopp"
 7z.exe a -tgzip -mx9 $Xopp $Xml
